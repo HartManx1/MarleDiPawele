6432f7ba5914ea4075e43a24c904503001b23498
Martin Hartman <hartmanx1@wp.pl>
2020-01-05 03:25:58 +0100

    nav anout and cards sections not working swiper

---
+++ README.md
@@ -0,0 +1,3 @@
+# new-Repo-Guide
+
+Link to the Page: https://inspirer10.github.io/new-Repo-Guide/
---
+++ js/slider.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
